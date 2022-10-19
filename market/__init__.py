115eca19c419d19e184afa7c0ad6a7435a4b5536
Abdullah-atia <abdo.atia632158@gmail.com>
2022-10-19 13:40:49 +0200

    upadate in stracture

---
+++ market/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
