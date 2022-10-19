3256a8a3e6b68a0dd189e386bf3db26498d6d5a0
Abdullah-atia <abdo.atia632158@gmail.com>
2022-10-19 13:53:51 +0200

    Modifiy __init__

--- market/__init__.py
+++ market/__init__.py
@@ -4,3 +4,5 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+from market import routes
