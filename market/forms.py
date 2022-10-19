c08fc44b65672a5816e12079f0374d81814b0e63
Abdullah-atia <abdo.atia632158@gmail.com>
2022-10-19 18:15:27 +0200

    add resiter.html file and my logo to app

--- market/forms.py
+++ market/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username')
-    email_address = StringField(label='email')
-    password1 =  PasswordField(label='password1')
-    password2 =  PasswordField(label='password2')
-    submit = SubmitField(label='submit')
+    username = StringField(label='User Name: ')
+    email_address = StringField(label='Email Address: ')
+    password1 =  PasswordField(label='Password: ')
+    password2 =  PasswordField(label='Confirm Password: ')
+    submit = SubmitField(label='Submit')
