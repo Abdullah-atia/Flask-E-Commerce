65b6159b18c251bbae52684344499bef6f37322f
Abdullah-atia <95718050+Abdullah-atia@users.noreply.github.com>
2022-10-19 13:03:29 +0200

    Add files via upload

---
+++ market.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+import os
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Item {self.name}'
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items = Item.query.all()
+    return render_template('market.html', items=items)
+
+if __name__ == '__main__':
+    # check if db file exists if not create it 
+    if not os.path.exists("./flaskblog/site.db"):
+        print("Database doesn't exist creating new one ...")
+        db.init_app(app)
+        with app.app_context():
+            db.create_all()
+
+    app.run(debug=True)
