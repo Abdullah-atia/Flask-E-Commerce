e7e73e72ad19be852d5d2fde8e01612307bc8271
Abdullah-atia <abdo.atia632158@gmail.com>
2022-10-21 22:35:54 +0200

    repair user model

--- market/forms.py
+++ market/forms.py
@@ -4,6 +4,6 @@ from wtforms import StringField, PasswordField, SubmitField
 class RegisterForm(FlaskForm):
     username = StringField(label='User Name: ')
     email_address = StringField(label='Email Address: ')
-    password1 =  PasswordField(label='Password: ')
-    password2 =  PasswordField(label='Confirm Password: ')
-    submit = SubmitField(label='Submit')
+    password1 = PasswordField(label='Password: ')
+    password2 = PasswordField(label='Confirm Password: ')
+    submit = SubmitField(label='Create Account')
