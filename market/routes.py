ca80bb6d50353ed46f5be1b1a990b3a393f4a355
Abdullah-atia <abdo.atia632158@gmail.com>
2022-10-24 17:20:53 +0200

    add log out page

--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -11,6 +11,7 @@ def home_page():
     return render_template('home.html')
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -43,3 +44,9 @@ def login_page():
             flash('username or password not match, Please try again', category='danger')
             
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out", category='info')
+    return redirect(url_for('home_page'))
