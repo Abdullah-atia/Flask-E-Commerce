421069da0860a2b5b2b045ed990cef558767f924
Abdullah-atia <abdo.atia632158@gmail.com>
2022-10-31 16:25:33 +0200

    add item_model or sell item part one

--- market/models.py
+++ market/models.py
@@ -16,6 +16,12 @@ class User(db.Model, UserMixin):
     items = db.relationship('Item', backref='owned_user', lazy=True) ###### understand #### ##relationship###
 
     @property
+    def prettier_budget(self):
+        if len(str(self.budget)) >= 4:
+            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}$'
+        else:
+            return f"{self.budget}$"
+    @property
     def password(self):
         return self.password
     
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm
 from market import db
 from flask_login import login_user, logout_user, login_required
 
@@ -10,11 +10,15 @@ from flask_login import login_user, logout_user, login_required
 def home_page():
     return render_template('home.html')
 
-@app.route('/market')
+@app.route('/market', methods=['GET', 'POST'])
 @login_required
 def market_page():
+    purchase_form = PurchaseItemForm()
+    if purchase_form.validate_on_submit():
+        print(request.form.get('purchased_item'))
+
     items = Item.query.all()
-    return render_template('market.html', items=items)
+    return render_template('market.html', items=items, purchase_form=purchase_form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
